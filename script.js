50bca5a568d8778c430b5c66e027a6af7aef5897
Elísio-Ferreira <elisio.ferreira.dev@gmail.com>
2024-02-22 21:04:04 -0300

    lojas lily´s

---
+++ script.js
@@ -0,0 +1,105 @@
+
+
+let clicado = document.querySelector(".imgGrande")
+let meuH2 = document.querySelector(".meuH2")
+let meuValor = document.querySelector(".meuValor")
+let meuCodigo = document.querySelector(".meuCodigo")
+
+
+let fuiClicado1 = document.querySelector(".fuiClicado1")
+let fuiClicado2 = document.querySelector(".fuiClicado2")
+let fuiClicado3 = document.querySelector(".fuiClicado3")
+let fuiClicado4 = document.querySelector(".fuiClicado4")
+let fuiClicado5 = document.querySelector(".fuiClicado5")
+let fuiClicado6 = document.querySelector(".fuiClicado6")
+let fuiClicado7 = document.querySelector(".fuiClicado7")
+let fuiClicado8 = document.querySelector(".fuiClicado8")
+let fuiClicado9 = document.querySelector(".fuiClicado9")
+let fuiClicado10 = document.querySelector(".fuiClicado10")
+
+
+fuiClicado1.addEventListener("click", () => {
+
+    clicado.src = ".img/colar banhado com imagem.png"
+    meuH2.innerText = " colar banhado com imagem"
+    meuValor.innerText ="Valor: 36,69 R$"
+    meuCodigo.innerText = "codigo: 7279"
+
+})
+
+
+fuiClicado2.addEventListener("click", () => {
+
+    clicado.src = "img/colar cheio.png"
+    meuH2.innerText = " colar cheio"
+    meuValor.innerText ="Valor: 57,69 R$"
+    meuCodigo.innerText = "codigo: 5234"
+})
+
+fuiClicado3.addEventListener("click", () => {
+
+    clicado.src = "img/colar de perola.png"
+    meuH2.innerText = " colar de perola"
+    meuValor.innerText ="Valor: 48,69 R$"
+    meuCodigo.innerText = "codigo: 6239"
+})
+
+fuiClicado4.addEventListener("click", () => {
+
+    clicado.src = "img/colar diamante verde.png"
+    meuH2.innerText = " colar de diamante verde"
+    meuValor.innerText ="Valor: 69,99 R$"
+    meuCodigo.innerText = "codigo: 6299"
+
+})
+
+fuiClicado5.addEventListener("click", () => {
+
+    clicado.src = "img/colar ouro ligitimo.png"
+    meuH2.innerText = " colar ouro ligitimo"
+    meuValor.innerText ="Valor: 99,99 R$"
+    meuCodigo.innerText = "codigo: 5673"
+
+})
+
+fuiClicado6.addEventListener("click", () => {
+
+    clicado.src = "img/colar ouro muciso.png"
+    meuH2.innerText = " colar ouro muciso"
+    meuValor.innerText ="Valor: 78,69 R$"
+    meuCodigo.innerText = "codigo: 9749"
+
+})
+
+
+fuiClicado7.addEventListener("click", () => {
+
+    clicado.src = "img/colar ultra fino.webp"
+    meuH2.innerText = " colar ultra fino"
+    meuValor.innerText ="Valor: 28,75 R$"
+    meuCodigo.innerText = "codigo: 3245"
+
+})
+
+
+fuiClicado8.addEventListener("click", () => {
+
+    clicado.src = "img/ouro banhado com cruz.png"
+
+
+})
+
+fuiClicado9.addEventListener("click", () => {
+
+    clicado.src = "img/ouro banhado.png"
+
+
+})
+
+fuiClicado10.addEventListener("click", () => {
+
+    clicado.src = "img/ouro medieval.png"
+
+
+})
+
